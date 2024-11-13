d1c1d4382532de782edd3ac8bf376dc9a7433cb7
Prem Osipalli <po@veritypay.com>
2024-11-13 16:00:02 -0600

    add hello controller

--- osipalli-template-services/osipalli-template-services-system/src/main/java/com/osipalli/template/services/system/SystemApplication.java
+++ osipalli-template-services/osipalli-template-services-system/src/main/java/com/osipalli/template/services/system/SystemApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OsipalliTemplateServicesSystemApplication {
+public class SystemApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(OsipalliTemplateServicesSystemApplication.class, args);
+        SpringApplication.run(SystemApplication.class, args);
     }
 
 }
