376f30668a1ae77b4c50382a7d31664ab28404aa
premosipalli <po@veritypay.com>
2024-11-13 22:26:33 -0600

    add oauth2

--- osipalli-template-common/src/main/java/com/spting/auth/config/JwtConfig.java
+++ osipalli-template-common/src/main/java/com/spting/auth/config/JwtConfig.java
@@ -1,5 +1,29 @@
 package com.spting.auth.config;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
 
+import java.util.List;
+
+@Component
+@ConfigurationProperties("spring.security.oauth2.resourceserver.jwt")
 public class JwtConfig {
+    private String issuerUri;
+    private List<String> whitelist;
+
+    public String getIssuerUri() {
+        return issuerUri;
+    }
+
+    public void setIssuerUri(String issuerUri) {
+        this.issuerUri = issuerUri;
+    }
+
+    public List<String> getWhitelist() {
+        return whitelist;
+    }
+
+    public void setWhitelist(List<String> whitelist) {
+        this.whitelist = whitelist;
+    }
 }
--- osipalli-template-services/osipalli-template-services-identity/src/main/java/com/osipalli/template/services/identity/IdentityApplication.java
+++ osipalli-template-services/osipalli-template-services-identity/src/main/java/com/osipalli/template/services/identity/IdentityApplication.java
@@ -2,12 +2,27 @@ package com.osipalli.template.services.identity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 
 @SpringBootApplication
 public class IdentityApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(IdentityApplication.class, args);
+    }
+
+    @Bean
+    public String run(){
+        String clientId = "client-credentials";
+        String clientSecret = "Black123";
+        String encodedCredentials = Base64.getEncoder().encodeToString((clientId+":"+clientSecret).getBytes(StandardCharsets.UTF_8));
+
+        System.out.println(encodedCredentials);
+
+        return encodedCredentials;
     }
 
 }
